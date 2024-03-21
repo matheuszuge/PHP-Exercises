821194d712bb64e3255e9150f1800927dbc03f17
matheuszuge <matheusrzuge@gmail.com>
2024-03-21 14:33:56 -0300

    Resolução de exercicios

--- 04_Leitura_doc_externo/index.php
+++ 04_Leitura_doc_externo/index.php
@@ -1,26 +1,97 @@
 <?php
 
-/*
-
------Como ler um arquivo externo----- 
+//-----Como ler um arquivo externo----- 
 
 $texto = file_get_contents('texto.txt');
 
 echo $texto;
 
+//-----Como modificar um arquivo----- 
 
------Como modificar um arquivo----- 
 $texto = 'matheus'
 file_put_contents('nome.txt', $texto);
 
 echo 'arquivo criado com sucesso';
 
-------------------------------------
+//------------------------------------
 
 file_get_contents('texto.txt');
 
 $texto .="/matheus"; Adicionando conteudo novo em $texto
 file_get_contents('texto.txt', $texto);
-*/
+
+//-----Excluindo aqruivos----
+
+unlink('texto.php');
+echo 'arquivo excluido';
+
+//-----Modificar aqruivos-----
+ 
+rename('texto', 'pasta/texto2.txt'); // o /pasta muda o documento de lugar. 
+copy('pasta/texto2.tx.', 'texto.txt')
+
+//-----uploud de arquivos-----
+
+//---Na Index---
+
+<form method="GET" action="recebedor.php" enctype="multipart/form-data">
+    <imput type="file" name:"arquivo"/>
+    <imput type="submit" name:"Enviar"/>
+</form>
+
+//----- no arquivo recebedor.php ----
+
+//- É quem vai receber e organizar o arquivo
+echo '<pre>';
+
+$nome = $_FILES['arquivos']['nome'];
+move_uploaded_file($_FILES['arquivo']['tmp_name'], 'arquivo/'.$nome);
+
+//-----Definição de nome aleatorio-----
+echo '<pre>';
+
+$nome = md5(time().rand(0, 1000).'.jpg');
+move_uploaded_file($_FILES['arquivo']['tmp_name'], 'arquivo/'.$nome);
+
+//-----formulario que somente aceita imagens, ou somente de um tipo ------
+
+echo '<pre>';
+
+$permitidos = array('image/jpeg', 'image/jpg', 'image/png')
+if (in_array($_FILES['arquivo']['type'], $permitidos)) {
+    $nome = md5(time().rand(0, 1000).'.jpg');
+    move_uploaded_file($_FILES['arquivo']['tmp_name'], 'arquivo/'.$nome);
+    echo 'arquivo salvo com sucesso';
+} else {
+    echo 'arquivo não permitido';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
---
+++ 05_Manipulacao_arrays/Index.php
@@ -0,0 +1,16 @@
+<?php
+//----------Array range---------
+
+$array = range(1, 20, 2);// terceiro parametro define quantos itens serão mostrados (de 2 em 2). 
+
+foreach($array as $item) {
+    echo $item."<br/>";
+}
+
+//-------------Array range com letras-----------
+
+$array = range("a", "g",) 
+
+foreach($array as $item) {
+    echo $item."<br/>";
+}
